3c70e47783887f7113a2387ab5b2dd2027a16e95
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2021-06-29 23:45:52 -0400

    Código Final da Aula 9.3 - Exclusão de Tarefas

--- src/br/edu/ifms/tasksapp/controllers/MainSceneController.java
+++ src/br/edu/ifms/tasksapp/controllers/MainSceneController.java
@@ -2,6 +2,7 @@ package br.edu.ifms.tasksapp.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import br.edu.ifms.tasksapp.dao.ITarefaDao;
@@ -12,6 +13,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -36,6 +40,10 @@ public class MainSceneController implements Initializable{
 		colDescricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
 		colPrioridade.setCellValueFactory(new PropertyValueFactory<>("prioridade"));
 		
+		carregaTarefas();
+	}
+	
+	private void carregaTarefas() {
 		ITarefaDao tarefaDao = new TarefaDao();
 		tableTarefas.setItems(FXCollections.observableArrayList( tarefaDao.buscarTodas() ));
 	}
@@ -49,6 +57,32 @@ public class MainSceneController implements Initializable{
 			primaryStage.setScene(scene);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void excluirTarefa() {
+		Tarefa tarefa = tableTarefas.getSelectionModel().getSelectedItem();
+		Alert alerta;
+		
+		if(tarefa != null) {
+			alerta = new Alert(AlertType.CONFIRMATION);
+			alerta.setTitle("Atenção");
+			alerta.setHeaderText("Confirmação de exclusão");
+			alerta.setContentText("Tem certeza que deseja excluir a tarefa de id "+tarefa.getId()+"?");
+			Optional<ButtonType> escolha = alerta.showAndWait();
+			
+			if(escolha.isPresent() && escolha.get() == ButtonType.OK ) {
+				ITarefaDao daoTarefa = new TarefaDao();
+				daoTarefa.excluir(tarefa);
+				
+				carregaTarefas();
+			}
+		}else {
+			alerta = new Alert(AlertType.INFORMATION);
+			alerta.setTitle("Atenção");
+			alerta.setHeaderText("Nenhuma tarefa selecionada!");
+			alerta.setContentText("Selecione a tarefa que deseja excluir.");
+			alerta.showAndWait();
 		}
 	}
 }
